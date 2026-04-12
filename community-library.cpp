0bb42eeb19e220be4a06efa92e09c28c3c84b577
Joao <joaovitorpimentagomes@gmail.com>
2026-04-12 15:16:15 +1000

    interface completion

--- community-library.cpp
+++ community-library.cpp
@@ -205,6 +205,93 @@ int main() {
     LIBRARY.addBook(book5);
     BookCollection SHELF = BookCollection(false);
 
+    // Main menu:
+    
+    vector<MenuOption> mainMenu = {
+        {"Library", [&]() {
+            vector<string> list = LIBRARY.getBookList();
+            string result = realtimeFlow("Library", list);
+            if (!result.empty()) {
+                int first = result.find(" | ");
+                string isbn = result.substr(0, first);
+                Book* book = nullptr;
+                for (int i = 0; i < LIBRARY.bookCount; i++) {
+                    if (LIBRARY.books[i].ISBN == isbn) {
+                        book = &LIBRARY.books[i];
+                        break;
+                    }
+                }
+                if (book != nullptr) {
+                    auto getInfo = [&]() {
+                        return "ISBN: " + book->ISBN + "\n"
+                            "Title: " + book->title + "\n"
+                            "Author: " + book->author + "\n"
+                            "Available: " + (book->isAvailable ? "Yes" : "No");
+                    };
+                    vector<MenuOption> bookMenu = {
+                        {"Change Title", [&]() {
+                            string newTitle = realtimeFlow("New title:", {});
+                            book->title = newTitle;
+                        }},
+                        {"Change Author", [&]() {
+                            string newAuthor = realtimeFlow("New author:", {});
+                            book->author = newAuthor;
+                        }},
+                        {"Borrow", [&]() {
+                            Book copy = *book;
+                            LIBRARY.removeBook(book->ISBN);
+                            SHELF.addBook(copy);
+                            displayMessage("Book borrowed!");
+                        }}
+                    };
+                    displayMenu(getInfo, bookMenu);
+                }
+            }
+        }},
+        {"Shelf", [&]() {
+            vector<string> list = SHELF.getBookList();
+            string result = realtimeFlow("Shelf", list);
+            if (!result.empty()) {
+                int first = result.find(" | ");
+                string isbn = result.substr(0, first);
+                Book* book = nullptr;
+                for (int i = 0; i < SHELF.bookCount; i++) {
+                    if (SHELF.books[i].ISBN == isbn) {
+                        book = &SHELF.books[i];
+                        break;
+                    }
+                }
+                if (book != nullptr) {
+                    auto getInfo = [&]() {
+                        return "ISBN: " + book->ISBN + "\n"
+                            "Title: " + book->title + "\n"
+                            "Author: " + book->author + "\n"
+                            "Available: " + (book->isAvailable ? "Yes" : "No");
+                    };
+                    vector<MenuOption> bookMenu = {
+                        {"Change Title", [&]() {
+                            string newTitle = realtimeFlow("New title:", {});
+                            book->title = newTitle;
+                        }},
+                        {"Change Author", [&]() {
+                            string newAuthor = realtimeFlow("New author:", {});
+                            book->author = newAuthor;
+                        }},
+                        {"Return", [&]() {
+                            Book copy = *book;
+                            SHELF.removeBook(book->ISBN);
+                            LIBRARY.addBook(copy);
+                            displayMessage("Book returned!");
+                        }}
+                    };
+                    displayMenu(getInfo, bookMenu);
+                }
+            }
+        }}
+    };
+
+    displayMenu([]() { return string("Main Menu"); }, mainMenu);
+    cout << "Thank you!" << endl;
     
     return 0;
 }
