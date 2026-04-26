b521b1ca1ff7ffbf353a6bdd170fc3ab16f480fc
Joao <joaovitorpimentagomes@gmail.com>
2026-04-27 05:19:30 +1000

    merge 1 and 2

--- community-library.cpp
+++ community-library.cpp
@@ -52,6 +52,14 @@ class BookCollection {
             bookCount = 0;
         }
 
+        vector<string> getBookList() {
+            vector<string> list;
+            for (int i = 0; i < bookCount; i++) {
+                list.push_back(books[i].ISBN + " | " + books[i].title + " | " + books[i].author + " | " + books[i].dateAdd);
+            }
+            return list;
+        }
+
         void addBook(Book bookToAdd) {
             bookToAdd.isAvailable = this->publicity;
             books[bookCount] = bookToAdd;
@@ -206,19 +214,232 @@ void insertionSort(Book arr[], int size) {
     cout << "[SORT] Sort complete." << endl;
 }
 
-// Main:
+// Interface logic:
 
-int main() {
+struct MenuOption {
+    string label;
+    function<void()> action;
+};
+
+char getKey() {
+    struct termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    char c = getchar();
+    if (c == 27) {
+        char next = getchar();
+        if (next == '[') c = getchar();
+        else c = 27;  
+    }
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return c;
+}
+
+void displayMessage(string message) {
+    system("clear");
+    cout << message << endl;
+    cout << "\nPress ESC twice to leave" << endl;
+
+    while (true) {
+        char key = getKey();
+        if (key == 27) break;
+    }
+}
+
+void displayMenu(function<string()> getTitle, vector<MenuOption> options) {
+    int selected = 0;
+    while (true) {
+        system("clear");
+        cout << "\n" << getTitle() << "\n" << endl;
+        for (int i = 0; i < options.size(); i++) {
+            if (i == selected) cout << " > " << options[i].label << endl;
+            else cout << "   " << options[i].label << endl;
+        }
+        cout << "\nPress ESC twice to leave" << endl;
+        char key = getKey();
+        if (key == 'A' && selected > 0) selected--;
+        else if (key == 'B' && selected < options.size() - 1) selected++;
+        else if (key == '\n' || key == '\r') options[selected].action();
+        else if (key == 27) break;
+    }
+}
+
+string realtimeFlow(string before, vector<string> items) {
+    string input = "";
+    int selected = -1;
+    vector<string> filtered;
+
+    while (true) {
+        filtered.clear();
+        for (string item : items) {
+            if (input.empty() || item.find(input) != string::npos) {
+                filtered.push_back(item);
+            }
+        }
+        if (selected >= filtered.size()) selected = filtered.size() - 1;
+
+        system("clear");
+        cout << before << endl;
+        cout << "> " << input << endl;
+
+        for (int i = 0; i < filtered.size(); i++) {
+            if (i == selected) cout << " > " << filtered[i] << endl;
+            else cout << "   " << filtered[i] << endl;
+        }
+
+        cout << "\nPress ESC twice to leave" << endl;
+
+        char key = getKey();
+
+        if (key == '\n' || key == '\r') {
+            if (selected >= 0 && !filtered.empty()) return filtered[selected];
+            return input; 
+        }
+        else if (key == 27) { return ""; }
+        else if (key == 'A') {  // seta cima
+            if (selected > 0) selected--;
+            else selected = -1;
+        }
+        else if (key == 'B') {  // seta baixo
+            if (selected < (int)filtered.size() - 1) selected++;
+        }
+        else if (key == 127) {  // backspace
+            if (!input.empty()) input.pop_back();
+            selected = -1;
+        }
+        else if (key >= 32 && key < 127) { 
+            input += key;
+            selected = -1;
+        }
+    }
+    return "";
+}
+
+
+// Assessment 1:
+
+int assessment1() {
+
+    // Sample data:
+
+    BookCollection LIBRARY = BookCollection(true);
+    Book book1 = Book("The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565", "01-04-2026");
+    Book book2 = Book("To Kill a Mockingbird", "Harper Lee", "978-0061120084", "01-04-2026");
+    Book book3 = Book("1984", "George Orwell", "978-0451524935", "01-04-2026"); 
+    Book book4 = Book("Moby Dick", "Herman Melville", "978-1503280786", "01-04-2026");
+    Book book5 = Book("Pride and Prejudice", "Jane Austen", "978-1503290563", "01-04-2026");
+    LIBRARY.addBook(book1);
+    LIBRARY.addBook(book2);
+    LIBRARY.addBook(book3);
+    LIBRARY.addBook(book4);
+    LIBRARY.addBook(book5);
+    BookCollection SHELF = BookCollection(false);
+
+    // Main menu:
+    
+    vector<MenuOption> mainMenu = {
+        {"Library", [&]() {
+            vector<string> list = LIBRARY.getBookList();
+            string result = realtimeFlow("Library", list);
+            if (!result.empty()) {
+                int first = result.find(" | ");
+                string isbn = result.substr(0, first);
+                Book* book = nullptr;
+                for (int i = 0; i < LIBRARY.bookCount; i++) {
+                    if (LIBRARY.books[i].ISBN == isbn) {
+                        book = &LIBRARY.books[i];
+                        break;
+                    }
+                }
+                if (book != nullptr) {
+                    auto getInfo = [&]() {
+                        return "ISBN: " + book->ISBN + "\n"
+                            "Title: " + book->title + "\n"
+                            "Author: " + book->author + "\n"
+                            "Available: " + (book->isAvailable ? "Yes" : "No");
+                    };
+                    vector<MenuOption> bookMenu = {
+                        {"Change Title", [&]() {
+                            string newTitle = realtimeFlow("New title:", {});
+                            book->title = newTitle;
+                        }},
+                        {"Change Author", [&]() {
+                            string newAuthor = realtimeFlow("New author:", {});
+                            book->author = newAuthor;
+                        }},
+                        {"Borrow", [&]() {
+                            Book copy = *book;
+                            LIBRARY.removeBook(book->ISBN);
+                            SHELF.addBook(copy);
+                            displayMessage("Book borrowed!");
+                        }}
+                    };
+                    displayMenu(getInfo, bookMenu);
+                }
+            }
+        }},
+        {"Shelf", [&]() {
+            vector<string> list = SHELF.getBookList();
+            string result = realtimeFlow("Shelf", list);
+            if (!result.empty()) {
+                int first = result.find(" | ");
+                string isbn = result.substr(0, first);
+                Book* book = nullptr;
+                for (int i = 0; i < SHELF.bookCount; i++) {
+                    if (SHELF.books[i].ISBN == isbn) {
+                        book = &SHELF.books[i];
+                        break;
+                    }
+                }
+                if (book != nullptr) {
+                    auto getInfo = [&]() {
+                        return "ISBN: " + book->ISBN + "\n"
+                            "Title: " + book->title + "\n"
+                            "Author: " + book->author + "\n"
+                            "Available: " + (book->isAvailable ? "Yes" : "No");
+                    };
+                    vector<MenuOption> bookMenu = {
+                        {"Change Title", [&]() {
+                            string newTitle = realtimeFlow("New title:", {});
+                            book->title = newTitle;
+                        }},
+                        {"Change Author", [&]() {
+                            string newAuthor = realtimeFlow("New author:", {});
+                            book->author = newAuthor;
+                        }},
+                        {"Return", [&]() {
+                            Book copy = *book;
+                            SHELF.removeBook(book->ISBN);
+                            LIBRARY.addBook(copy);
+                            displayMessage("Book returned!");
+                        }}
+                    };
+                    displayMenu(getInfo, bookMenu);
+                }
+            }
+        }}
+    };
+
+    displayMenu([]() { return string("Main Menu"); }, mainMenu);
+    cout << "Thank you!" << endl;
+    
+    return 0;
+}
+
+// Assessment 2:
+int assessment2() {
 
     // --- Correct books ---
-    Book correct1 = Book("The Great Gatsby",      "F. Scott Fitzgerald", "978-0743273565", "2024-01-10");
-    Book correct2 = Book("To Kill a Mockingbird", "Harper Lee",          "978-0061120084", "2024-02-15");
-    Book correct3 = Book("1984",                  "George Orwell",       "978-0451524935", "2024-03-20");
+    Book correct1 = Book("The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565", "2024-01-10");
+    Book correct2 = Book("To Kill a Mockingbird", "Harper Lee", "978-0061120084", "2024-02-15");
+    Book correct3 = Book("1984","George Orwell", "978-0451524935", "2024-03-20");
 
     // --- Incorrect books ---
-    Book incorrect1 = Book("",      "Unknown Author", "INVALID-ISBN",    "99/99/9999");
-    Book incorrect2 = Book("12345", "",               "",                "");
-    Book incorrect3 = Book("!@#$%", "???",            "000-0000000000",  "not-a-date");
+    Book incorrect1 = Book("", "Unknown Author", "INVALID-ISBN", "99/99/9999");
+    Book incorrect2 = Book("12345", "",  "", "");
+    Book incorrect3 = Book("!@#$%", "???", "000-0000000000", "not-a-date");
 
     cout << "========== CORRECT BOOKS ==========" << endl;
     cout << "\n[1]" << endl; correct1.display();
@@ -252,4 +473,8 @@ int main() {
     cout << "\n========== END OF TESTS ==========" << endl;
 
     return 0;
+}
+
+int main() {
+    assessment2();
 }
